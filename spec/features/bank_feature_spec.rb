2e85561b142ea075b6384b2bd3a281aeb81cce5c
Simone <simonecaplin@gmail.com>
2018-07-16 11:02:43 +0100

    Write user stories

---
+++ spec/features/bank_feature_spec.rb
@@ -0,0 +1,14 @@
+# User Story 1
+# As a user
+# So that I can avoid carrying too much cash
+# I want to be able to deposit money in my bank account and see the deposit reflected in my balance
+
+# User Story 2
+# As a user
+# So that I can fill my wallet
+# I want to be able to withdraw money from my bank account and see the withdrawal reflected in my balance
+
+# User Story 3
+# As a user
+# So that I can track my spending
+# I want to print an account summary which details my transactions, balance and dates
