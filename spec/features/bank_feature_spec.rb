3eac0d653d634b4a6173baee33392dff09b855f1
Simone <simonecaplin@gmail.com>
2018-07-16 14:57:20 +0100

    Rename spec file

--- spec/features/bank_feature_spec.rb
+++ spec/features/bank_feature_spec.rb
@@ -20,13 +20,3 @@ describe "Printing a bank statement" do
     expect(account.print_statement).to eq %Q(date || credit || debit || balance\n16/07/2018 || 20 ||  || 25 || \n15/07/2018 ||  || 5 || 5 || \n14/07/2018 || 10 ||  || 10 || )
   end
 end
-
-# User Story 2
-# As a user
-# So that I can avoid carrying too much cash
-# I want to be able to deposit money in my bank account and see the deposit reflected in my balance
-
-# User Story 3
-# As a user
-# So that I can fill my wallet
-# I want to be able to withdraw money from my bank account and see the withdrawal reflected in my balance
