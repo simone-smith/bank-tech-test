1c7ecf55e64e3252f6a4499042a1af096f019484
Simone <simonecaplin@gmail.com>
2018-07-16 22:02:38 +0100

    Fix Rubocop issues

--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -13,8 +13,8 @@ describe TransactionHistory do
   end
 
   it "stores a withdrawal in the log" do
-    transaction_history.add_transaction({ date: date, credit: 30, debit: "", balance: 30})
-    transaction_history.add_transaction({ date: date, credit: "", debit: 15, balance: 15})
+    transaction_history.add_transaction({ date: date, credit: 30, debit: "", balance: 30 })
+    transaction_history.add_transaction({ date: date, credit: "", debit: 15, balance: 15 })
 
     expect(transaction_history.log).to eq [{ date: date, credit: 30, debit: "", balance: 30 }, { date: date, credit: "", debit: 15, balance: 15 }]
   end
