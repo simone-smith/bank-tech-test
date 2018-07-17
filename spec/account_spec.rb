0e77988f2c32b33b0b3ce36eb52ac60c1fdb1631
Simone <simonecaplin@gmail.com>
2018-07-17 16:02:42 +0100

    Edit account spec to test for return value of deposit and withdraw methods

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,24 +5,44 @@ describe Account do
   subject(:account) { Account.new(transaction_history, printer) }
 
   let(:transaction_history) { double :transaction_history, :log => [] }
-
+  let(:transaction_log) { [] }
   let(:printer) { double :printer, print_statement: "date || credit || debit || balance\n16/07/2018 ||  || 15.00 || 15.00 || \n16/07/2018 || 30.00 ||  || 30.00\n" }
 
   it "starts with a balance of 0" do
     expect(account.balance).to eq 0
   end
 
-  it "updates the balance when money is deposited" do
-    allow(transaction_history).to receive(:add_transaction)
-    account.deposit(10)
-    expect(account.balance).to eq 10
+  context 'deposit' do
+
+    before do
+      allow(transaction_history).to receive(:add_transaction) { transaction_log }
+    end
+
+    it "updates the balance when money is deposited" do
+      account.deposit(10)
+      expect(account.balance).to eq 10
+    end
+
+    it "returns the transaction log when money is deposited" do
+      expect(account.deposit(10)).to eq transaction_log
+    end
   end
 
-  it "updates the balance when money is withdrawn" do
-    allow(transaction_history).to receive(:add_transaction)
-    account.deposit(30)
-    account.withdraw(15)
-    expect(account.balance).to eq 15
+  context 'withdraw' do
+
+    before do
+      allow(transaction_history).to receive(:add_transaction) { transaction_log }
+    end
+
+    it "updates the balance when money is withdrawn" do
+      account.deposit(30)
+      account.withdraw(15)
+      expect(account.balance).to eq 15
+    end
+
+    it "returns the transaction log when money is withdrawn" do
+      expect(account.withdraw(10)).to eq transaction_log
+    end
   end
 
   it "prints a statement" do
