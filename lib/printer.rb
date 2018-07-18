1b8c090b5410dcc9c90934175d4a2d3bf98e7d30
Simone <simonecaplin@gmail.com>
2018-07-18 09:57:26 +0100

    Refactor row into private method

--- lib/printer.rb
+++ lib/printer.rb
@@ -5,11 +5,7 @@ class Printer
   DELIMITER = ' || '
 
   def print_statement(log)
-    row = ""
-    log.reverse_each do |transaction|
-      row << "#{transaction[:date].strftime("%d/%m/%Y")} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]}\n"
-    end
-    print header + row
+    print header + row(log)
   end
 
   private
@@ -18,4 +14,18 @@ class Printer
     HEADER.join(DELIMITER) + "\n"
   end
 
+  def row(log)
+    row = ""
+    log.reverse_each do |transaction|
+      row << "#{transaction[:date].strftime("%d/%m/%Y")} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]}\n"
+    end
+    row
+  end
+
 end
+
+# Use print/puts once.
+#
+# 1. knowledge of the header, and formatting
+# 2. it loops through the array
+# 3. has knowledge of the row formatting
