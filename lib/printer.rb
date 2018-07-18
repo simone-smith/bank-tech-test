82776799117b940b3bb6c022f7b74321f1ca8e05
Simone <simonecaplin@gmail.com>
2018-07-18 09:26:39 +0100

    Extract formatting of header to private method

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,12 +1,20 @@
 require 'date'
 
 class Printer
+  HEADER = %w(date credit debit balance)
+  DELIMITER = ' || '
 
   def print_statement(log)
-    print "date || credit || debit || balance\n"
+    print header + "\n"
     log.reverse_each do |transaction|
       print "#{transaction[:date].strftime("%d/%m/%Y")} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]}\n"
     end
+  end
+
+  private
+
+  def header
+    HEADER.join(DELIMITER)
   end
 
 end
